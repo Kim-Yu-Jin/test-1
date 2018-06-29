701c2f273735fe44e514ed1b8eda079d5fbcd13a
Kim-Yu-Jin <40683368+Kim-Yu-Jin@users.noreply.github.com>
2018-06-29 15:17:42 +0900

    김유진
    메인 자료형 수정 / 단 정렬

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-  int i,j;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
-    }
-  }
-  
-  return 0;
-}
+	int i, j;
 
-// comment
+	for (i = 1; i < 5; i++) {
+		for (j = 1; j < 10; j++) {
+			printf("\n%d x %d = %d ", i, j, i * j);
+		}
+	}
+
+	getchar();
+
+}
